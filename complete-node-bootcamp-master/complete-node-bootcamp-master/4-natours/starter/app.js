3bfc1f6d891e29c6aec708f2614c786291201d7e
lukethorvilson <luke.thorvilson@wsu.edu>
2024-05-25 17:07:30 -0700

    Created my very first api get method in Express!

--- complete-node-bootcamp-master/complete-node-bootcamp-master/4-natours/starter/app.js
+++ complete-node-bootcamp-master/complete-node-bootcamp-master/4-natours/starter/app.js
@@ -1,16 +1,36 @@
-const express = require('express');
+// Core Modules
+const fs = require('fs');
 
+//Import Express and create express app
+const express = require('express');
+const { dirname } = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello form the server side!', names: ['Luke'] });
-});
+// Setting Up Express and Running a basic Server Lession
+{
+  // app.get('/', (req, res) => {
+  //   res
+  //     .status(200)
+  //     .json({ message: 'Hello form the server side!', names: ['Luke'] });
+  // });
+  // app.post('/', () => {
+  //     res.send('You can post to this endpoint...');
+  // })
+}
 
-app.post('/', () => {
-    res.send('You can post to this endpoint...');
-})
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
 
 const port = 8000;
 app.listen(port, () => {
