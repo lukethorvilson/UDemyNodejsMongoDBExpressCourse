ae31d22650946a77910d31cbdfdb77feb5bb9bf9
lukethorvilson <luke.thorvilson@wsu.edu>
2024-05-28 16:03:56 -0700

    Added some mongoose code for creatin g a new model and schema.

--- complete-node-bootcamp-master/complete-node-bootcamp-master/4-natours/starter/server.js
+++ complete-node-bootcamp-master/complete-node-bootcamp-master/4-natours/starter/server.js
@@ -1,9 +1,57 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({path: './config.env'});
+dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+); //replacing password placeholder with the config.env database password
 
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log('DB connection successful');
+  })
+  .catch((err) => {
+    console.error(err.message);
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Forest Hiker',
+  rating: 4.7,
+  price: 497,
+});
+
+testTour.save().then(doc => {
+  console.log(doc);
+}).catch(err =>{
+  console.error(`💥${err.message}`);
+});
 
 // START SERVER
 const port = process.env.PORT || 3000;
