c224de5ad5e9afe8e32f8f3e957c2781bc165599
lukethorvilson <luke.thorvilson@wsu.edu>
2024-05-30 16:16:51 -0700

    Created a duplicate field handler for the DB.

--- complete-node-bootcamp-master/complete-node-bootcamp-master/4-natours/starter/controllers/errorController.js
+++ complete-node-bootcamp-master/complete-node-bootcamp-master/4-natours/starter/controllers/errorController.js
@@ -1,9 +1,15 @@
-const AppError = require("./../util/appError");
+const AppError = require('./../util/appError');
 
-const handleCastErrorDB = err => {
-    const message = `Invalid ${err.path}: ${err.value}`
-    return new AppError(message, 400);
-}
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 404);
+};
 
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -24,7 +30,7 @@ const sendErrorProd = (err, res) => {
     // Programming or other unknown error: dont leak error details
   } else {
     //1) Log Error
-    console.error('Error 💥', err)
+    console.error('Error 💥', err);
     // 2) Send generic message
     res.status(500).json({
       status: 'error',
@@ -40,10 +46,11 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'prodcution') {
-    let error = {...err};
+    let error = { ...err };
 
-    if(error.name === "CastError") error = handleCastErrorDB(err) 
-    
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+
     sendErrorProd(error, res);
   }
 };
