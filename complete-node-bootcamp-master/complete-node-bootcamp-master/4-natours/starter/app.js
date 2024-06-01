1c57b0123f184a71c1ee56ed91bc145a19ea96d2
lukethorvilson <luke.thorvilson@wsu.edu>
2024-06-01 15:59:46 -0700

    Here we imported some middleware in which can defend from NoSQL Query injections and sanatization.

--- complete-node-bootcamp-master/complete-node-bootcamp-master/4-natours/starter/app.js
+++ complete-node-bootcamp-master/complete-node-bootcamp-master/4-natours/starter/app.js
@@ -1,6 +1,11 @@
 // Core Modules
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanatize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+
 const AppError = require('./util/appError');
 const globalErrorHandler = require('./controllers/errorController');
 // IMPORTED ROUTERS
@@ -12,13 +17,30 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // MIDDLEWARES
+// Security HTTP headers
+app.use(helmet());
 
 // only use logger when in development env
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+// Body Parser
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanatize());
+// Data santization against XSS
+app.use(xss());
+
 app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
