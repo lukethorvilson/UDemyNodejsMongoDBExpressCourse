347cfaf60fe359b153accb20459d40f75713da3f
lukethorvilson <luke.thorvilson@wsu.edu>
2024-06-02 21:25:46 -0700

    Created a method of referecning children

--- complete-node-bootcamp-master/complete-node-bootcamp-master/4-natours/starter/models/tourModel.js
+++ complete-node-bootcamp-master/complete-node-bootcamp-master/4-natours/starter/models/tourModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-const validator = require('validator');
+const User = require('./../models/userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -104,6 +104,12 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+      },
+    ],
   },
   {
     toJSON: { virtuals: true },
@@ -121,6 +127,11 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
+  next();
+});
 // tourSchema.pre('save', function(next) {
 //   console.log('Will save document...');
 //   next();
