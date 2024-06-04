3f7211f2ca76c95a99693823705dc7b0ea8a81bf
lukethorvilson <luke.thorvilson@wsu.edu>
2024-06-03 19:43:58 -0700

    Added indexing to the tour model.

--- complete-node-bootcamp-master/complete-node-bootcamp-master/4-natours/starter/models/tourModel.js
+++ complete-node-bootcamp-master/complete-node-bootcamp-master/4-natours/starter/models/tourModel.js
@@ -117,6 +117,9 @@ const tourSchema = new mongoose.Schema(
   },
 );
 
+// tourSchema.index({ price: 1 });
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({slug: 1});
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
@@ -125,7 +128,7 @@ tourSchema.virtual('durationWeeks').get(function () {
 tourSchema.virtual('reviews', {
   ref: 'Review',
   foreignField: 'tour',
-  localField: '_id'
+  localField: '_id',
 });
 
 // DOCUMENT MIDDLEWARE: runs before .save() and .create() .insertMany
