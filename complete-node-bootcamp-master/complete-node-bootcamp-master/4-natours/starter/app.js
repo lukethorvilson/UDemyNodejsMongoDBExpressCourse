8b28b24cd1147fe8bc6da256a757eda1ca12aa76
lukethorvilson <luke.thorvilson@wsu.edu>
2024-06-05 15:17:19 -0700

    Set up structure for section.

--- complete-node-bootcamp-master/complete-node-bootcamp-master/4-natours/starter/app.js
+++ complete-node-bootcamp-master/complete-node-bootcamp-master/4-natours/starter/app.js
@@ -14,6 +14,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 //Import Express and create express app
 const app = express();
@@ -77,9 +78,11 @@ app.get('/tour', (req, res) => {
 });
 
 // ROUTE MOUNTS
+app.use('/', viewRouter)
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+
 
 app.get('/', (req, res) => {
   res.status(200).render('base', { tour: 'The Forest Hiker', user: 'Jonas' });
